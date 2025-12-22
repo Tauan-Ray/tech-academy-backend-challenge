12d727442afc2afd4c96cbf4643c6a5d138f14e9
Tauan-Ray <tauanray995@gmail.com>
2025-12-22 14:28:42 -0300

    feat(student): adicionado domínio base de student

---
+++ student-service/src/main/kotlin/com/techacademy/student/domain/model/Student.kt
@@ -0,0 +1,12 @@
+package com.techacademy.student.domain.model
+
+import java.time.LocalDateTime
+
+data class Student(
+    val id: Int? = null,
+    var name: String,
+    var email: String,
+    var createdAt: LocalDateTime? = null,
+    var updatedAt: LocalDateTime? = null,
+    var deletedAt: LocalDateTime? = null,
+)
---
+++ student-service/src/main/kotlin/com/techacademy/student/domain/repository/StudentRepositoryPort.kt
@@ -0,0 +1,9 @@
+package com.techacademy.student.domain.repository
+
+import com.techacademy.student.domain.model.Student
+
+interface StudentRepositoryPort {
+    fun findAll(): List<Student>
+    fun findStudent(id: Int): Student?
+    fun findStudentByEmail(email: String): Student?
+}
