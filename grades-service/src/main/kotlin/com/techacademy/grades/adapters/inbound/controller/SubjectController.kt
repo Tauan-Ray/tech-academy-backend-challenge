c04ad86ed9e1b930ea5bfdf9c438ce8f77965a6a
Tauan-Ray <tauanray995@gmail.com>
2025-12-23 15:12:01 -0300

    feat(subject): adicionado controller REST para consulta de disciplinas

---
+++ grades-service/src/main/kotlin/com/techacademy/grades/adapters/inbound/controller/SubjectController.kt
@@ -0,0 +1,42 @@
+package com.techacademy.grades.adapters.inbound.controller
+
+import com.techacademy.grades.application.dto.SubjectDTO
+import com.techacademy.grades.application.usecase.subject.FindAllSubjectsUseCase
+import com.techacademy.grades.application.usecase.subject.FindSubjectByNameUseCase
+import com.techacademy.grades.application.usecase.subject.FindSubjectUseCase
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.QueryParam
+import jakarta.ws.rs.core.MediaType
+
+@Path("/subjects")
+class SubjectController(
+    private val findAllSubjectsUseCase: FindAllSubjectsUseCase,
+    private val findSubjectUseCase: FindSubjectUseCase,
+    private val findSubjectByNameUseCase: FindSubjectByNameUseCase,
+) {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun findAll(): List<SubjectDTO> {
+        return findAllSubjectsUseCase
+            .execute()
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun findSubject(@PathParam("id") id: Int): SubjectDTO? {
+        return findSubjectUseCase
+            .execute(id)
+    }
+
+    @GET
+    @Path("/name")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun findStudentByEmail(@QueryParam("name") name: String): SubjectDTO? {
+        return findSubjectByNameUseCase
+            .execute(name)
+    }
+}
