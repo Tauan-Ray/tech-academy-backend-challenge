fa8f29b856fe9d17e194a1d0d67f67f76088bedb
Tauan-Ray <tauanray995@gmail.com>
2025-12-27 18:38:08 -0300

    feat(enrollment): adicionado repository para enrollments

---
+++ student-service/src/main/kotlin/com/techacademy/student/domain/repository/EnrollmentRepositoryPort.kt
@@ -0,0 +1,10 @@
+package com.techacademy.student.domain.repository
+
+import com.techacademy.student.domain.model.Enrollment
+
+interface EnrollmentRepositoryPort {
+    fun findAll(): List<Enrollment>
+    fun findById(enrollmentId: Int): Enrollment?
+    fun findByStudent(studentId: Int): List<Enrollment>
+    fun findByClassroom(classroomId: Int): List<Enrollment>
+}
